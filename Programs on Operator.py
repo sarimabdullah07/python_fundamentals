8b0cbb8be115446af36162387a2bcdba07f9d896
unknown <sarimabdullah242@gmail.com>
2026-09-06 15:23:31 +0530

    temprature converter added

--- Programs on Operator.py
+++ Programs on Operator.py
@@ -1,3 +1,8 @@
 print("Area of a circle")
 r=int(input("Enter radius of a circle: "))
 print(f"Area of a circle is {round((2*(22/7))*(r**2),2)}")
+
+print("Temprature Converter")
+t=float(input("Enter temprature in degree celcius: "))
+print(f"{t}° celsius = {t*(9/5)+32}° Fahrenheit. ")
+print(f"{t}° celsius = {t+273.15}° Kelvin")
